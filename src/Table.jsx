c0c9c339521d1a2ad47c0c79e4c26e0b1501d88d
Zeynep Dağdeviren <av.zeynepdagd@gmail.com>
2024-04-17 22:17:17 +0300

    feat: searchfilter yapıldı

---
+++ src/Table.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Table = ({ data }) => {
+
+
+    return (
+
+        <table>
+
+            <tbody>
+
+                <tr>
+                    <th>Name</th>
+                    <th>Surname</th>
+                    <th>E-mail</th>
+
+                </tr>
+
+                {data.map((item) => (
+
+                    <tr key={item.id}>
+                        <td>{item.first_name}</td>
+                        <td>{item.last_name}</td>
+                        <td>{item.email}</td>
+
+                    </tr>
+
+
+
+
+                ))}
+
+            </tbody>
+        </table>
+
+    )
+}
+
+export default Table 
