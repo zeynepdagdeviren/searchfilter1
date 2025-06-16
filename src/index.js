262eb88fd2c7054bbd597773c7f700b62206493b
zeynepdagdeviren <zdag.globaldev@gmail.com>
2025-06-16 12:20:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './app.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -7,5 +8,4 @@ root.render(
     <App />
  
 );
-
 
