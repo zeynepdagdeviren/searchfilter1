6d60e3f8d141ab5f87a8dda30d1fe97155198923
zeynepdagdeviren <zdag.globaldev@gmail.com>
2025-06-16 12:19:34 +0300

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,41 +1,30 @@
 import { useState } from "react";
 import "./App.css";
 import { Users } from "./users";
-import Table from "./table";
+import Table from "./Table";
 
 function App() {
+  const [query, setQuery] = useState("");
 
-    const [query, setQuery] = useState("")
+  const keys = ["first_name", "last_name", "email"];
 
-
-    const keys = ["first_name", "last_name", "email"]
-
-
-
-    const search = (data) => {
-        return data.filter( 
-            (item) =>
-                keys.some(key => item[key].toLowerCase().includes(query))
-
-         );
-    };
-
-
-
-
-       return (
-        <div className="app">
-
-            <input type="text"
-                placeholder="Search..."
-                className="search"
-                onChange={(e) => setQuery(e.target.value)} />
-
-                <Table data={search(Users)}/> 
-            
-        </div>
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query.toLowerCase()))
     );
-}
+  };
 
+  return (
+    <div className="app">
+      <input
+        type="text"
+        placeholder="Search..."
+        className="search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <Table data={search(Users)} />
+    </div>
+  );
+}
 
 export default App;
